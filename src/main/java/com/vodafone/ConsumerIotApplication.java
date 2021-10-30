fab0a94c1b6aef08d29d116cd8c91f550966d267
AmitRoshan <aamit2811@gamil.com>
2021-10-30 21:17:18 +0530

    initial commit

---
+++ src/main/java/com/vodafone/ConsumerIotApplication.java
@@ -0,0 +1,13 @@
+package com.vodafone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerIotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumerIotApplication.class, args);
+	}
+
+}
